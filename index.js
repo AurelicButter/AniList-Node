417922be86d164bf75bd64bc815bd05ad3480ce1
Butterstroke <konirosen@protonmail.ch>
2018-06-13 20:38:30 -0400

    Start

---
+++ index.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+var query = `query ($id: Int) { # Define which variables will be used in the query (id)
+    Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+      id
+      title {
+        romaji
+        english
+        native
+      }
+    }
+  }`,
+variables = {
+    id: 15125
+},
+url = 'https://graphql.anilist.co',
+options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        query: query,
+        variables: variables
+    })
+};
+
+fetch(url, options).then(handleResponse)
+                   .then(handleData)
+                   .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+    console.log(data);
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
---
+++ queryBuilder.js
@@ -0,0 +1,21 @@
+function queryBuilder() {
+    switch (type) {
+        case "anime":
+            var query = {
+
+            }
+            break;
+        case "manga":
+            break;
+        case "character":
+            break;
+        case "staff":
+            break;
+        case "user":
+            break;
+        default: 
+            break;
+    }
+};
+
+module.exports = queryBuilder;
