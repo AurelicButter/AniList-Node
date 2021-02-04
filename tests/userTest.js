6854992765c9416206fc55f79b73938bb3e2eee9
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-02-04 00:22:45 -0400

    Revert "Improved JSDoc Comments for Intellisense"
    This reverts commit 01e7ff0611f5fcd0f64d81a14ca2831dfd5519dc.

--- tests/userTest.js
+++
@@ -1,5 +0,0 @@
-const AnilistNode = require("../lib/index.js");
-const anilist = new AnilistNode();
-
-// Fetch a user's profile
-anilist.user.profile("Butterstroke").then(data => console.log(data));
