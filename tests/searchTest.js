433c445c17643142d227fc0a5a1de5657faf710b
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-02-28 21:38:25 -0400

    Fixed ActivityFilterTypes Mistype & 1.7.0 Cleanup
    Fixed a type with ActivityFilterTypes definition. Forgot an I.
    
    Updated documentation by adding a new tutorial for filtering and using it as well as to have the documentation generated for release.

--- tests/searchTest.js
+++ tests/searchTest.js
@@ -1,4 +1,14 @@
 const anilistnode = require("../lib/index");
 const Anilist = new anilistnode();
 
+// Depreciated Search Method
 Anilist.search("anime", "Occultic;Nine", 1, 10).then((data) => { console.log(data); });
+
+
+let myFilter = {
+    isAdult: true,
+    source_in: ["ORIGINAL", "LIGHT_NOVEL"]
+};
+
+// New Search Method
+Anilist.searchEntry.anime(null, myFilter).then(data => console.log(data));
