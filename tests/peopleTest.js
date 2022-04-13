57e175c0b943782855c35d08fc19aebc01af4f72
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-04-13 04:08:44 -0300

    Added Function to Favourite Characters

--- tests/peopleTest.js
+++ tests/peopleTest.js
@@ -1,14 +1,26 @@
 const AnilistNode = require("../lib/index.js");
-const anilist = new AnilistNode();
+const { token } = require("../token.json");
+const anilist = new AnilistNode(token);
 
 //Find staff via their Anilist ID
-anilist.people.staff(95672).then(data => { console.log(data); });
+anilist.people.staff(95672).then((data) => {
+	console.log(data);
+});
 
 //Finds staff via their names.
-anilist.people.staff("Yuuki Kaji").then(data => { console.log(data); });
+anilist.people.staff("Yuuki Kaji").then((data) => {
+	console.log(data);
+});
 
 //Finds a character via their Anilist ID
-anilist.people.character(88344).then(data => { console.log(data); });
+anilist.people.character(88344).then((data) => {
+	console.log(data);
+});
 
 //Finds a character via their name.
-anilist.people.character("Yuuta Gamon").then(data => { console.log(data); });
+anilist.people.character("Yuuta Gamon").then((data) => {
+	console.log(data);
+});
+
+// Adds/Removes favourite from characters
+anilist.people.favouriteChar(126156).then((data) => console.log(data));
